ec0bdcf29b04e811a9da010ae0e8fa435e1eacfb
mridul56 <munmridul@gmail.com>
2024-06-08 13:43:48 +0600

    refactored heat index model

--- dbt_unit_testing_refactored/models/iot/intermediate/int_heat_indexes.sql
+++ dbt_unit_testing_refactored/models/iot/intermediate/int_heat_indexes.sql
@@ -1,3 +1,6 @@
+{% set sensor_info = 'sensor_info' %}
+{% set hi_coefficients = 'hi_coefficients' %}
+
 SELECT 
     sensor_info.device_name,
     sensor_info.manufacturer,
@@ -8,7 +11,19 @@ SELECT
     sensor_info.temperature AS temperature_in_celsius,
     sensor_info.temperature_in_fahrenheit,
     humidity,
-    {{ heat_index_in_fahrenheit(hi_coefficients_model='hi_coefficients',sensor_info_model='sensor_info') }} AS feels_like_in_fahrenheit,
-    {{ convert_to_celcius(heat_index_in_fahrenheit= heat_index_in_fahrenheit(hi_coefficients_model='hi_coefficients',sensor_info_model='sensor_info')) }} AS feels_like_in_celsius
-FROM {{ ref('int_all_sensor_info') }} AS sensor_info,
-{{ ref('stg_heat_index_coefficients')}} as hi_coefficients
+    {{ 
+        heat_index_in_fahrenheit(
+            hi_coefficients_model=hi_coefficients,
+            sensor_info_model=sensor_info
+        ) 
+    }} AS feels_like_in_fahrenheit,
+    {{ 
+        convert_to_celcius(
+            heat_index_in_fahrenheit=heat_index_in_fahrenheit(
+                hi_coefficients_model=hi_coefficients,
+                sensor_info_model=sensor_info
+            )
+        ) 
+    }} AS feels_like_in_celsius
+FROM {{ ref('int_all_sensor_info') }} AS {{ sensor_info }},
+{{ ref('stg_heat_index_coefficients')}} as {{ hi_coefficients }}
